0d4e75de74cc2078d069ea5d2d696d4cfa4aed40
insidepower <insidepower@greengrassl.(none)>
2010-04-19 21:54:08 +0800

    ms_gui: with dialog box

--- ms_gui/menu_icon_static/main.c
+++ ms_gui/menu_icon_static/main.c
@@ -3,88 +3,138 @@
 
 const char g_szClassName[] = "myWindowClass";
 
+BOOL CALLBACK AboutDlgProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
+{
+	switch(Message)
+	{
+		case WM_INITDIALOG:
+
+		return TRUE;
+		case WM_COMMAND:
+			switch(LOWORD(wParam))
+			{
+				case IDOK:
+					EndDialog(hwnd, IDOK);
+				break;
+				case IDCANCEL:
+					EndDialog(hwnd, IDCANCEL);
+				break;
+			}
+		break;
+		default:
+			return FALSE;
+	}
+	return TRUE;
+}
+
 // Step 4: the Window Procedure
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-    switch(msg)
-    {
-        case WM_LBUTTONDOWN:
-// BEGIN NEW CODE
-        {
-            char szFileName[MAX_PATH];
-            HINSTANCE hInstance = GetModuleHandle(NULL);
+	switch(msg)
+	{
+		case WM_LBUTTONDOWN:
+			// BEGIN NEW CODE
+			{
+				char szFileName[MAX_PATH];
+				HINSTANCE hInstance = GetModuleHandle(NULL);
 
-            GetModuleFileName(hInstance, szFileName, MAX_PATH);
-            MessageBox(hwnd, szFileName, "This program is:", MB_OK | MB_ICONINFORMATION);
-        }
-// END NEW CODE
-        break;
-        case WM_CLOSE:
-            DestroyWindow(hwnd);
-        break;
-        case WM_DESTROY:
-            PostQuitMessage(0);
-        break;
-        default:
-            return DefWindowProc(hwnd, msg, wParam, lParam);
-    }
-    return 0;
+				GetModuleFileName(hInstance, szFileName, MAX_PATH);
+				MessageBox(hwnd, szFileName, "This program is:", MB_OK | MB_ICONINFORMATION);
+			}
+			// END NEW CODE
+		/// menu id
+		case WM_COMMAND:
+			switch(LOWORD(wParam))
+			{
+				case ID_FILE_EXIT:
+					PostMessage(hwnd, WM_CLOSE, 0, 0);
+					break;
+				case ID_HELP_ABOUT:
+					{
+						int ret = DialogBox(GetModuleHandle(NULL), 
+								MAKEINTRESOURCE(IDD_ABOUT), hwnd, AboutDlgProc);
+						if(ret == IDOK){
+							MessageBox(hwnd, "Dialog exited with IDOK.", "Notice",
+									MB_OK | MB_ICONINFORMATION);
+						}
+						else if(ret == IDCANCEL){
+							MessageBox(hwnd, "Dialog exited with IDCANCEL.", "Notice",
+									MB_OK | MB_ICONINFORMATION);
+						}
+						else if(ret == -1){
+							MessageBox(hwnd, "Dialog failed!", "Error",
+									MB_OK | MB_ICONINFORMATION);
+						}
+					}
+					break;
+			}
+			break;
+		case WM_CLOSE:
+			DestroyWindow(hwnd);
+			break;
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+		default:
+			return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+	return 0;
 }
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine, int nCmdShow)
+		LPSTR lpCmdLine, int nCmdShow)
 {
-    WNDCLASSEX wc;
-    HWND hwnd;
-    MSG Msg;
+	WNDCLASSEX wc;
+	HWND hwnd;
+	MSG Msg;
 
-    //Step 1: Registering the Window Class
-    wc.cbSize        = sizeof(WNDCLASSEX);
-    wc.style         = 0;
-    wc.lpfnWndProc   = WndProc;
-    wc.cbClsExtra    = 0;
-    wc.cbWndExtra    = 0;
-    wc.hInstance     = hInstance;
-    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-    wc.lpszMenuName  = MAKEINTRESOURCE(IDR_MYMENU);
-    wc.lpszClassName = g_szClassName;
-    wc.hIcon  = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON));
-    wc.hIconSm  = (HICON)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON), IMAGE_ICON, 16, 16, 0);
+	//Step 1: Registering the Window Class
+	wc.cbSize        = sizeof(WNDCLASSEX);
+	wc.style         = 0;
+	wc.lpfnWndProc   = WndProc;
+	wc.cbClsExtra    = 0;
+	wc.cbWndExtra    = 0;
+	wc.hInstance     = hInstance;
+	wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	wc.lpszMenuName  = MAKEINTRESOURCE(IDR_MYMENU);
+	wc.lpszClassName = g_szClassName;
+	wc.hIcon  = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON));
+	wc.hIconSm  = (HICON)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MYICON), IMAGE_ICON, 16, 16, 0);
 
 
-    if(!RegisterClassEx(&wc))
-    {
-        MessageBox(NULL, "Window Registration Failed!", "Error!",
-            MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
+	if(!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, "Window Registration Failed!", "Error!",
+				MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
 
-    // Step 2: Creating the Window
-    hwnd = CreateWindowEx(
-        WS_EX_CLIENTEDGE,
-        g_szClassName,
-        "The title of my window",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
-        NULL, NULL, hInstance, NULL);
+	// Step 2: Creating the Window
+	hwnd = CreateWindowEx(
+			WS_EX_CLIENTEDGE,
+			g_szClassName,
+			"The title of my window",
+			WS_OVERLAPPEDWINDOW,
+			CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
+			NULL, NULL, hInstance, NULL);
 
-    if(hwnd == NULL)
-    {
-        MessageBox(NULL, "Window Creation Failed!", "Error!",
-            MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
+	if(hwnd == NULL)
+	{
+		MessageBox(NULL, "Window Creation Failed!", "Error!",
+				MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
 
-    ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
 
-    // Step 3: The Message Loop
-    while(GetMessage(&Msg, NULL, 0, 0) > 0)
-    {
-        TranslateMessage(&Msg);
-        DispatchMessage(&Msg);
-    }
-    return Msg.wParam;
+	// Step 3: The Message Loop
+	while(GetMessage(&Msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
+	}
+	return Msg.wParam;
 }
--- ms_gui/menu_icon_static/resource.h
+++ ms_gui/menu_icon_static/resource.h
@@ -4,17 +4,23 @@
 //
 #define IDR_MYMENU                      101
 #define IDI_MYICON                      102
+#define IDD_DIALOGBAR                   103
+#define IDD_ABOUT                       104
+#define IDC_BUTTON1                     1000
+#define IDC_BUTTON2                     1001
 #define ID_FILE_EXIT                    40001
 #define ID_STUFF_GO                     40002
 #define ID_STUFF_GOSOMEWHEREELSE        40003
+#define ID_HELP_ABOUT                   40004
+#define IDC_STATIC                      -1
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
-#define _APS_NEXT_COMMAND_VALUE         40004
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_COMMAND_VALUE         40005
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
