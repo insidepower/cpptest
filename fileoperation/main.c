c9562bfc385031a948d5d58abaf6fc9c0b331de9
Ng Khian Nam <Khian.Nam.Ng@continental-corporation.com>
2011-02-23 10:06:56 +0800

    improved read binary

--- fileoperation/main.c
+++ fileoperation/main.c
@@ -29,10 +29,25 @@ int main(void){
 	}
 	printf("\n");
 	fclose(fp);
+
+	fp = fopen("IndData2.txt", "rb");
+	fseek(fp, -234, SEEK_END);
+	char time[5];
+	printf("reading3: ");
+	for (i=0; i<5; i++){
+		fread(&time[i], sizeof(char), 1, fp);
+	}
+	printf("%02x %02x %02x %02x %02x\n",
+			time[0],
+			time[1],
+			time[2],
+			time[3],
+			time[4]);
 	return 0;
 }
 
 /* result:
 reading: 01 02 03 04 05 06 07 08
 reading2: 04 05 06 07 08
+reading3: 02 23 09 08 59
  */
