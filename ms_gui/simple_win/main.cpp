dd1502f95d3aa040990246c0a819cf3ab287731c
unknown <uidc1325@.cw01.contiwan.com>
2011-05-31 18:12:12 +0800

    performance lousy, no button shown

--- ms_gui/simple_win/main.cpp
+++ ms_gui/simple_win/main.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+//#include "stdafx.h"
 #include <windows.h>
 #include "resource.h"
 #define MAX_LOADSTRING 100
@@ -19,7 +19,6 @@ int WINAPI WinMain(HINSTANCE hInstance,
 				   LPTSTR     lpCmdLine,
 				   int       nCmdShow)
 {
-	// TODO: Place code here.
 	MSG msg;
 	HACCEL hAccelTable;
 
@@ -64,14 +63,15 @@ int WINAPI WinMain(HINSTANCE hInstance,
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
 	WNDCLASS wc;
-	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.style = CS_HREDRAW | CS_VREDRAW;;
 	wc.lpfnWndProc = (WNDPROC) WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
 	wc.hIcon = 0;
 	wc.hCursor = 0;
-	wc.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH);
+	//wc.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH);
+	wc.hbrBackground =(HBRUSH) GetStockObject(LTGRAY_BRUSH);
 	wc.lpszMenuName = 0;
 	wc.lpszClassName = szWindowClass;
 	return RegisterClass(&wc);
@@ -93,7 +93,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 	HWND hWnd;
 	hInst = hInstance; // Store instance handle in our global variable
 	hWnd = CreateWindow(szWindowClass, szTitle, WS_BORDER | WS_SYSMENU,
-		0, 0, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+		CW_USEDEFAULT, CW_USEDEFAULT, 500, 230, NULL, NULL, hInstance, NULL);
 	if (!hWnd)
 	{
 		return FALSE;
@@ -102,6 +102,27 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 	UpdateWindow(hWnd);
 	//MessageBox(hWnd, TEXT("my title"), TEXT("header"),NULL);
 	return TRUE;
+}
+
+BOOL CALLBACK DialogBoxProc(HWND hWnd, UINT Message,
+							   WPARAM wParam, LPARAM lParam)
+{
+	switch(Message)
+	{
+		case WM_COMMAND:
+			switch(LOWORD(wParam)){
+				case IDC_BUTTON1: 
+					MessageBox(hWnd, TEXT("btn1 is pressed"), TEXT("header"),NULL);
+					break; 
+				case IDC_BUTTON2: break; 
+				default:
+					return FALSE;
+			}
+		case WM_NEXTDLGCTL:
+			break;
+		default:
+			return FALSE;
+	}
 }
 
 //
@@ -114,12 +135,11 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_DESTROY  - post a quit message and return
 //
 //
-int xCor[5];
-int yCor[5];
-int curIndex = 0;
-int totalClick = 0;
-RECT rt;
-#define TOTAL 5
+/*-------------------------------------------------------------------------
+ Global Variables
+ -------------------------------------------------------------------------*/
+HWND g_hModelessDlgBox = NULL;
+HWND hStatus = NULL;
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	int i=0;
@@ -129,31 +149,25 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	LoadString(hInst, IDS_HELLO, szHello, MAX_LOADSTRING);
 	switch (message) 
 	{
+	case WM_CREATE:
+		/// create child window
+		{
+			g_hModelessDlgBox = CreateDialog(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_FORMVIEW),
+				hWnd, DialogBoxProc);
+			if(g_hModelessDlgBox != NULL) {
+				ShowWindow(g_hModelessDlgBox, SW_SHOW);
+			} else {
+				MessageBox(hWnd, "CreateDialog returned NULL", "Warning!",  
+					MB_OK | MB_ICONEXCLAMATION);
+			}
+		}
+		break;
+	case WM_INITDIALOG:
+		break;
 	case WM_LBUTTONDOWN:
-		// RECT rt;
-		//   GetClientRect(hWnd, &rt);
-		xCor[curIndex] = LOWORD(lParam);
-		yCor[curIndex] = HIWORD(lParam);
-
-		curIndex = (curIndex+1)%TOTAL;
-		InvalidateRect(hWnd, &rt, TRUE);
-		if(totalClick<5)
-			totalClick++;
 		break;
 
 	case WM_PAINT:
-		hdc = BeginPaint(hWnd, &ps);
-		// TODO: Add any drawing code here...
-
-		GetClientRect(hWnd, &rt);
-		DrawText(hdc, szHello, _tcslen(szHello), &rt, DT_CENTER);
-		//Ellipse(hdc, 10-TOTAL, 10-TOTAL, 
-		//  10+TOTAL, 10+TOTAL);
-		for (i=0; i<totalClick; i++) {
-			Ellipse(hdc, xCor[i]-TOTAL, yCor[i]-TOTAL, 
-				xCor[i]+TOTAL, yCor[i]+TOTAL);
-		}
-		EndPaint(hWnd, &ps);
 		break;
 
 	case WM_DESTROY:
--- ms_gui/simple_win/resource.h
+++ ms_gui/simple_win/resource.h
@@ -2,10 +2,12 @@
 // Microsoft Visual C++ generated include file.
 // Used by mywin.rc
 //
-#define IDD_DIALOG1                     101
+#define IDD_FORMVIEW                    101
 #define IDS_APP_TITLE                   102
 #define IDS_WIN_CLASS                   103
 #define IDS_HELLO                       104
+#define IDC_BUTTON1                     1001
+#define IDC_BUTTON2                     1002
 
 // Next default values for new objects
 // 
@@ -13,7 +15,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
